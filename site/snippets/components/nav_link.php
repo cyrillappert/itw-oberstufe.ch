206580dc8d4ceb220944edcbd3cd3f1c49fd4150
Cyrill Appert <cyrill@phasewas.ch>
2025-01-06 23:08:39 +0100

    added underline for hovered menu items

--- site/snippets/components/nav_link.php
+++ site/snippets/components/nav_link.php
@@ -1,10 +1,12 @@
 <a
-    <?= $p->isActive() ? 'class="underline pointer-events-none"' : '' ?>
+    class="hover:underline <?= $p->isActive() ? 'underline pointer-events-none' : '' ?>"
     href="<?= $p->url() ?>"
     hx-push-url="true"
     hx-swap="none"
     hx-get="<?= $p->hxPath() ?>"
     :class="{ 'underline pointer-events-none': active === '<?= $p->slug() ?>' }"
     @click="active = '<?= $p->slug() ?>'">
+
     <?= $p->title() ?>
+
 </a>
