fc9ef3d162119bed1132688797ae92178415cfe4
Cyrill Appert <cyrill@phasewas.ch>
2025-01-09 13:24:24 +0100

    Removed field group again and instead extended the default blueprint

--- site/templates/default.php
+++ site/templates/default.php
@@ -9,20 +9,19 @@
 <?php slot('main') ?>
 
 <main class="self-end bg-yellow" id="main" hx-swap-oob="true">
-    <?= $page->text()->md() ?>
-</main>
-
-<div class="self-end justify-self-end w-80" id="featured-image" hx-swap-oob="true">
-    <?php if ($image = $page->hero_image()->toFile()): ?>
-        <?php snippet('components/image', ['f' => $image->crop(640)]) ?>
+    <?php if ($image = $page->featured_image()->toFile()): ?>
+        <div class="self-end justify-self-end w-80" id="featured-image" hx-swap-oob="true">
+            <?php snippet('components/image', ['f' => $image->crop(640)]) ?>
+        </div>
     <?php endif ?>
-    <h2>
-        <?= $page->hero_headline() ?>
+    <h2 class="font-bold">
+        <?= $page->headline() ?>
     </h2>
     <div>
-        <?= $page->hero_text() ?>
+        <?= $page->blurp() ?>
     </div>
-</div>
+</main>
+
 
 <?php endslot() ?>
 
