67d6dac51ae4c69addcae74edaa7923a966214ca
Cyrill Appert <cyrill@phasewas.ch>
2025-01-09 23:45:17 +0100

    This is the first thing I don't like. I added a hook that adds tailwind classes to the html.

--- site/config/config.php
+++ site/config/config.php
@@ -1,4 +1,38 @@
 <?php
 return [
     'debug' => true,
+    'hooks' => [
+        'kirbytext:after' => function ($text) {
+
+            // Config RTE classes
+
+            $from = array();
+            $from[0] = '/<h1>/';
+            $from[1] = '/<h2>/';
+            $from[2] = '/<h3>/';
+            $from[3] = '/<strong>/';
+            $from[4] = '/<em>/';
+            $from[5] = '/<p>/';
+            $from[6] = '/<code>/';
+            $from[7] = '/<ul>/';
+            $from[8] = '/<ol>/';
+            $from[9] = '/<li>/';
+
+            $to = array();
+            $to[0] = '<h1 class="text-6xl">';
+            $to[1] = '<h2 class="text-4xl">';
+            $to[2] = '<h3 class="text-2xl">';
+            $to[3] = '<strong class="">';
+            $to[4] = '<em class="">';
+            $to[5] = '<p class="">';
+            $to[6] = '<code class="">';
+            $to[7] = '<ul class="">';
+            $to[8] = '<ol class="">';
+            $to[9] = '<li class="">';
+
+            $result = preg_replace($from, $to, $text);
+
+            return $result;
+        }
+    ]
 ];
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,17 +1,18 @@
 <?php snippet('layout', slots: true) ?>
 <?php slot('main') ?>
 
-<main class="self-end bg-yellow-400" id="main" hx-swap-oob="true">
-    <?php if ($image = $page->featured_image()->toFile()): ?>
-        <div class="self-end justify-self-end w-80" id="featured-image" hx-swap-oob="true">
-            <?php snippet('components/image', ['f' => $image->crop(640)]) ?>
-        </div>
-    <?php endif ?>
-    <h2 class="font-bold">
-        <?= $page->headline() ?>
-    </h2>
-    <div>
-        <?= $page->blurp() ?>
+<main class="self-end " id="main" hx-swap-oob="true">
+    <div class="bg-red-500">
+        <?php if ($image = $page->featured_image()->toFile()): ?>
+            <div class="self-end justify-self-end w-80" id="featured-image" hx-swap-oob="true">
+                <?php snippet('components/image', ['f' => $image->crop(640)]) ?>
+            </div>
+        <?php endif ?>
+        <?= $page->headline()->kt() ?>
+    </div>
+    <div class="bg-orange-500">
+        <?= $page->text()->kt() ?>
+
     </div>
 </main>
 
