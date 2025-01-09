3bb0560f55a16ee570b0cdd70650d29a56c9e63b
Cyrill Appert <cyrill@phasewas.ch>
2025-01-09 10:58:22 +0100

    Added menu, that can be edited

--- site/snippets/partials/nav.php
+++ site/snippets/partials/nav.php
@@ -1,6 +1,6 @@
 <nav x-data="{ active: '<?= $page->slug() ?>' }" class="justify-self-end">
     <ul>
-        <?php foreach ($pages->listed() as $p): ?>
+        <?php foreach ($site->menu()->toPages() as $p): ?>
             <li>
                 <?= snippet('components/nav_link', ['p' => $p]) ?>
             </li>
